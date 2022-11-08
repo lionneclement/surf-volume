4fac577483255899e34764d64769c08878c636df
lionneclement <lionneclement@gmail.com>
2022-11-08 15:07:40 +0800

    refeacto jest test

--- __tests__/IdealVolumeCalculator.test.tsx
+++ __tests__/IdealVolumeCalculator.test.tsx
@@ -1,45 +1,7 @@
 import 'react-native';
 import {skillRange} from '../app/data/range/SkillRangeData';
 import {WeightRange, weightRange} from '../app/data/range/WeighRangeData';
-import {
-  idealVolumeCalculator,
-  surfboardVolumeCalculator,
-} from '../app/utils/VolumeCalculator';
-
-describe('Should calculate the volume of a surfboard', () => {
-  it('Shortboard', () => {
-    expect(
-      surfboardVolumeCalculator({
-        length: 175.26,
-        width: 50.1,
-        thickness: 6.3,
-        shapeValue: 0.54,
-      }),
-    ).toBe(29.9);
-  });
-
-  it('Hybrid/Fish', () => {
-    expect(
-      surfboardVolumeCalculator({
-        length: 175.26,
-        width: 53.9,
-        thickness: 6.9,
-        shapeValue: 0.56,
-      }),
-    ).toBe(36.5);
-  });
-
-  it('Funboard/Mal', () => {
-    expect(
-      surfboardVolumeCalculator({
-        length: 279.4,
-        width: 59.7,
-        thickness: 9.5,
-        shapeValue: 0.58,
-      }),
-    ).toBe(91.9);
-  });
-});
+import {idealVolumeCalculator} from '../app/utils/VolumeCalculator';
 
 describe('Should calculate your board volume', () => {
   const lightWeightRange = weightRange.filter(
